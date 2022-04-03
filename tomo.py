47e3b59db9ca95c65552bef36fc9be00a333ba48
Mattie <matthew.hall@serenetangerine.com>
2022-04-02 18:41:35 -0600

    refactored code making tomo a class and removed unnecessary duplicate sprites

--- tomo.py
+++ tomo.py
@@ -33,6 +33,47 @@ def cpuTemp():
     return temp
 
 
+class Tomo:
+    def __init__(self):
+        directory = os.path.dirname(__file__)
+        # load sprites
+        self.tomo_left = Image.open('%s/sprites/tomo.bmp' % directory).convert('1')
+        self.tomo_right = self.tomo_left.transpose(Image.FLIP_LEFT_RIGHT)
+        self.tomo_left_sweat = Image.open('%s/sprites/tomo_sweat.bmp' % directory).convert('1')
+        self.tomo_right_sweat = self.tomo_left_sweat.transpose(Image.FLIP_LEFT_RIGHT)
+
+        # default sprite
+        self.tomo_sprite = self.tomo_left
+
+        # initial coordinates
+        self.x = 50
+        self.y = 34
+
+    def walk(self, temp):
+        dir = random.randint(0, 6)
+        if dir == 0:
+            pass
+        elif dir % 2 == 1:
+            if float(temp) > 50.0:
+                self.tomo_sprite = self.tomo_left_sweat
+            else:
+                self.tomo_sprite = self.tomo_left
+            if self.x > 0:
+                self.x = self.x - 2
+        elif dir % 2 == 0:
+            if float(temp) > 50.0:
+                self.tomo_sprite = self.tomo_right_sweat
+            else:
+                self.tomo_sprite = self.tomo_right
+            if self.x < 100:
+                self.x = self.x + 2
+        
+        if self.x % 3 == 0:
+            self.y = 32
+        else:
+            self.y = 34
+
+
 ## get arguments
 args = getArguments()
 
@@ -47,29 +88,22 @@ disp.begin()
 disp.clear()
 disp.display()
 
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+## create image
+image = Image.new('1', (disp.width, disp.height))
 draw = ImageDraw.Draw(image)
-draw.rectangle((0,0, width, height), outline=0, fill=0)
 
+## initialize font
 fontsize = 13
 font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuS/DejaVuSans.ttf', fontsize)
 
-## load images
-directory = os.path.dirname(__file__)
-tomo_left = Image.open('%s/sprites/tomo_left.bmp' % directory).convert('1')
-tomo_right = Image.open('%s/sprites/tomo_right.bmp' % directory).convert('1')
-tomo_left_sweat = Image.open('%s/sprites/tomo_left_sweat.bmp' % directory).convert('1')
-tomo_right_sweat = Image.open('%s/sprites/tomo_right_sweat.bmp' % directory).convert('1')
 
-## set initial position
-(x, y) = (50, 34)
+## tomo is born!
+tomo = Tomo()
 
-## start walk
+## start loop
 while True:
-    draw.rectangle((0,0, width, height), outline=0, fill=0)
-    
+    draw.rectangle((0,0, disp.width, disp.height), outline=0, fill=0)
+   
     # cpu temp
     temp = cpuTemp()
     if args.temp:
@@ -81,30 +115,11 @@ while True:
         time = datetime.now().strftime('%H:%M')
         draw.text((90, 7), time, font=font, fill=255)
 
-    dir = random.randint(0, 6)
-    if dir == 0:
-        pass
-    elif dir % 2 == 1:
-        if float(temp) > 50.0:
-            tomo = tomo_left_sweat
-        else:
-            tomo = tomo_left
-        if x > 0:
-            x = x - 2
-    elif dir % 2 == 0:
-        if float(temp) > 50.0:
-            tomo = tomo_right_sweat
-        else:
-            tomo = tomo_right
-        if x < 100:
-            x = x + 2
+    # start walk
+    tomo.walk(temp)
     
-    if x % 3 == 0:
-        y = 32
-    else:
-        y = 34
 
-    image.paste(tomo, (x, y))
+    image.paste(tomo.tomo_sprite, (tomo.x, tomo.y))
     disp.image(image)
     disp.display()
     sleep(0.5)
