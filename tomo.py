a1a5ba03bc5ee0a61a458bd2b8bfd6fc19590d8b
Mattie <matthew.hall@serenetangerine.com>
2022-04-02 19:25:06 -0600

    added egg hatch start animation

--- tomo.py
+++ tomo.py
@@ -35,8 +35,8 @@ def cpuTemp():
 
 class Tomo:
     def __init__(self):
-        directory = os.path.dirname(__file__)
         # load sprites
+        directory = os.path.dirname(__file__)
         self.tomo_left = Image.open('%s/sprites/tomo.bmp' % directory).convert('1')
         self.tomo_right = self.tomo_left.transpose(Image.FLIP_LEFT_RIGHT)
         self.tomo_left_sweat = Image.open('%s/sprites/tomo_sweat.bmp' % directory).convert('1')
@@ -46,13 +46,14 @@ class Tomo:
         self.tomo_sprite = self.tomo_left
 
         # initial coordinates
-        self.x = 50
-        self.y = 34
+        (self.x, self.y) = (50, 34)
 
     def walk(self, temp):
         dir = random.randint(0, 6)
+        # do nothing if 0
         if dir == 0:
             pass
+        # walk left if odd
         elif dir % 2 == 1:
             if float(temp) > 50.0:
                 self.tomo_sprite = self.tomo_left_sweat
@@ -60,6 +61,7 @@ class Tomo:
                 self.tomo_sprite = self.tomo_left
             if self.x > 0:
                 self.x = self.x - 2
+        # walk right if even
         elif dir % 2 == 0:
             if float(temp) > 50.0:
                 self.tomo_sprite = self.tomo_right_sweat
@@ -72,6 +74,40 @@ class Tomo:
             self.y = 32
         else:
             self.y = 34
+
+
+class Egg:
+    def __init__(self):
+        # load sprites
+        directory = os.path.dirname(__file__)
+        self.egg1 = Image.open('%s/sprites/egg1.bmp' % directory).convert('1')
+        self.egg2 = Image.open('%s/sprites/egg2.bmp' % directory).convert('1')
+        self.egg3 = Image.open('%s/sprites/egg3.bmp' % directory).convert('1')
+
+        # default sprite
+        self.egg_sprite = self.egg1
+
+        # initial coordinates
+        (self.x, self.y) = (50, 34)
+
+        self.count = 0
+
+    def idle(self):
+        if self.count % 2 == 0:
+            self.egg_sprite = self.egg1
+        else:
+            self.egg_sprite = self.egg2
+
+        self.count = self.count + 1
+
+    def hatch(self):
+        if self.count %2 == 0:
+            self.egg_sprite = self.egg1
+        else:
+            self.egg_sprite = self.egg3
+
+        self.count = self.count + 1
+
 
 
 ## get arguments
@@ -95,6 +131,23 @@ draw = ImageDraw.Draw(image)
 ## initialize font
 fontsize = 13
 font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuS/DejaVuSans.ttf', fontsize)
+
+
+## animate egg hatching
+egg = Egg()
+while egg.count <= 30:
+    egg.idle()
+    image.paste(egg.egg_sprite, (egg.x, egg.y))
+    disp.image(image)
+    disp.display()
+    sleep(0.5)
+
+while egg.count <= 35:
+    egg.hatch()
+    image.paste(egg.egg_sprite, (egg.x, egg.y))
+    disp.image(image)
+    disp.display()
+    sleep(0.5)
 
 
 ## tomo is born!
