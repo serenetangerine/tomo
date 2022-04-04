7c478cdf2588d63522f0a5d0b16642117cfa4d6f
Mattie <matthew.hall@serenetangerine.com>
2022-04-04 10:48:04 -0600

    tweaks to food spawn rate

--- tomo.py
+++ tomo.py
@@ -322,7 +322,7 @@ def main():
 
         # spawn food
         if not food.spawned:
-            if random.randint(0, 75) == 0:
+            if random.randint(0, 50) == 0:
                 food.spawn(tomo.x)
     
         # start walk
